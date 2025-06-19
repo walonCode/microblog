3eed3a43da750a0455b02d846f3a20d7b9687364
Mohamed Lamin Walon Jalloh <mohamedlaminwalonjalloh@gmail.com>
2025-06-19 17:11:31 +0000

    Add user registration route (#2)
    * add the user login auth route
    
    * refactor: auth token
    
    ---------
    
    Co-authored-by: Sparrow <benjaminthorpe19@gmail.com>

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -1,12 +1,36 @@
+import bcrypt from "bcryptjs";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
-import { nanoid } from "nanoid";
+import * as jwt from "hono/jwt";
+
+import { config } from "../config/config.ts";
+import { db } from "../db/drizzle.ts";
+import { usersTable } from "../db/schema.ts";
 
 const app = new Hono().basePath("/auth");
 
 app.post("/token", async (c) => {
   const body = await c.req.json();
 
-  return c.json({ data: { token: nanoid(), ...body } });
+  const user = await db.query.usersTable.findFirst({
+    where: eq(usersTable.username, body.username),
+    columns: { id: true, password: true },
+  });
+
+  try {
+    if (!user || !(await bcrypt.compare(body.password, user.password))) {
+      return c.json({ error: "invalid username or password" }, 401);
+    }
+
+    const token = await jwt.sign(
+      { id: user.id, exp: 24 * 60 * 60 * 1 },
+      config.JWT_SECRET_KEY,
+    );
+
+    return c.json({ data: { token } });
+  } catch (_e: unknown) {
+    return c.json({ error: _e.message }, 500);
+  }
 });
 
 export default app;
